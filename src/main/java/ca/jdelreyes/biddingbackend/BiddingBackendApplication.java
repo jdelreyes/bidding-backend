77b79a01ccdd8050e98a9dcf1985a39f2453a457
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-03-27 03:21:56 -0400

    initial commit

---
+++ src/main/java/ca/jdelreyes/biddingbackend/BiddingBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.jdelreyes.biddingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiddingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/jdelreyes/biddingbackend/BiddingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jdelreyes.biddingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
